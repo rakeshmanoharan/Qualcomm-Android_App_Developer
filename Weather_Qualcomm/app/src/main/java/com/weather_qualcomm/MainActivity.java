5406e836e1eb2f7b642673ca92d0412bdebf92fc
Rakesh Manoharan <rakeshmanoharan@ymail.com>
2016-02-10 15:38:08 -0600

    Requirement 1 Complete - Random Numbers for 5 days

--- Weather_Qualcomm/app/src/main/java/com/weather_qualcomm/MainActivity.java
+++ Weather_Qualcomm/app/src/main/java/com/weather_qualcomm/MainActivity.java
@@ -5,28 +5,55 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    //region static variables
+    private static String TAG = "MainActivity";
+    private Random randomGenerator;
+    //endregion
 
+    //region activity lifecycle's
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Log.d(TAG, "onCreate..");
+        //Random object is created to generate random numbers for all the 5 days
+        randomGenerator = new Random();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Log.d(TAG, "Generating Random numbers");
+        //Generate temparature in celcius as random numbers
+        TextView monday = (TextView) findViewById(R.id.tmon);
+        monday.setText(generateRandom());
+
+        TextView tuesday = (TextView) findViewById(R.id.ttue);
+        tuesday.setText(generateRandom());
+
+        TextView wednesday = (TextView) findViewById(R.id.twed);
+        wednesday.setText(generateRandom());
+
+        TextView thursday = (TextView) findViewById(R.id.tthu);
+        thursday.setText(generateRandom());
+
+        TextView friday = (TextView) findViewById(R.id.tfri);
+        friday.setText(generateRandom());
+
+        Log.d(TAG, "Random numbers generated");
     }
+
+    private String generateRandom() {
+        // Random Generator generates the value from 0 to 36
+        // The range of celcius values possible is from -6 to 30
+        return String.valueOf(randomGenerator.nextInt(36) - 6);
+    }
+    //endregion
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
