2d3cd73ab35fb1489161b2c0dd63cffe198febad
Rakesh Manoharan <rakeshmanoharan@ymail.com>
2016-02-10 23:38:00 -0600

    Ambient Temperature - Sensor Implementation

--- Weather_Qualcomm/app/src/main/java/com/weather_qualcomm/MainActivity.java
+++ Weather_Qualcomm/app/src/main/java/com/weather_qualcomm/MainActivity.java
@@ -1,30 +1,56 @@
 package com.weather_qualcomm;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     //region static variables
     private static String TAG = "MainActivity";
-    private Random randomGenerator;
     //endregion
+
+    //region Variables
+    private Random randomGenerator;
+    private SensorManager mSensorManager;
+    private Sensor mAmbientTemperatureSensor;
+    private boolean sensorExists = true;
+    private TextView temperature_air;
+    //endregion
+
+    static {
+        System.loadLibrary("myCelciusToFarenheit");
+    }
+
+//    public native String convertTemperature();
 
     //region activity lifecycle's
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate..");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate..");
+
+        // Code to work with sensors
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        temperature_air = (TextView) findViewById(R.id.temperature);
+        // Sensors introduced on Ice Cream Sandwitch level but our minimum SDK support is API 21
+        if (Build.VERSION.SDK_INT >= 21) {
+            mAmbientTemperatureSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        }
+        if (mAmbientTemperatureSensor == null) {
+            sensorExists = false;
+            Log.d(TAG,"Sensor doesn't exist in this phone");
+            temperature_air.setText("Sensor doesn't exist");
+        }
+
         //Random object is created to generate random numbers for all the 5 days
         randomGenerator = new Random();
 
@@ -48,32 +74,52 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Random numbers generated");
     }
 
+    // Sensor should register on create and also on resume of the activity
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorExists) {
+            mSensorManager.registerListener(this, mAmbientTemperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG,"Register Ambient Temperature Sensor");
+        }
+    }
+
+    // Sensors should be unregistered when going on pause or stop
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorExists) {
+            mSensorManager.unregisterListener(this);
+            Log.d(TAG, "Unregister Ambient Temperature Sensor");
+        }
+    }
+    //endregion
+
+    //region override methods
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float temperature_air = event.values[0];
+        updateTemperatureUI(temperature_air);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    //endregion
+
+    //region user defined functions
     private String generateRandom() {
         // Random Generator generates the value from 0 to 36
         // The range of celcius values possible is from -6 to 30
         return String.valueOf(randomGenerator.nextInt(36) - 6);
     }
+
+    private void updateTemperatureUI(float temperature) {
+        temperature_air.setText(String.valueOf(temperature_air));
+        Log.d(TAG, "Ambient Temperature updated on UI");
+    }
     //endregion
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
